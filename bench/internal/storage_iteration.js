42692e7755eaa0dc442b62cefcf5e2c40b30638a
Florent Cailhol <florent.cailhol@gmail.com>
2020-01-02 16:17:20 +0100

    Benchmark another storage implementation

--- bench/internal/storage_iteration.js
+++ bench/internal/storage_iteration.js
@@ -23,17 +23,18 @@ const { suite } = require("../utils");
  * Results on node v13.5.0:
  *
  *   Array of Structures
- *    baseline: 348,988 op/s (±0.53%)
- *    chunks: 283,002 op/s (±0.48%)
+ *     baseline: 329,066 op/s (±0.62%)
+ *     chunks: 283,225 op/s (±0.43%)
  *
- *  Structure of Arrays
- *    baseline: 353,105 op/s (±0.52%)
- *    chunks: 310,852 op/s (±0.76%)    [FASTEST]
- *    lookup: 167,793 op/s (±1.09%)
+ *   Structure of Arrays
+ *     baseline: 367,183 op/s (±0.48%)
+ *     chunks: 312,989 op/s (±0.78%)
+ *     lookup: 174,355 op/s (±0.42%)    [SLOWEST]
  *
- *  Flat array
- *    baseline: 354,142 op/s (±0.49%)
- *    lookup: 166,462 op/s (±0.49%)    [SLOWEST]
+ *   Flat array
+ *     baseline: 365,895 op/s (±0.63%)
+ *     chunks: 331,122 op/s (±0.51%)    [FASTEST]
+ *     lookup: 166,684 op/s (±0.55%)    [SLOWEST]
  *
  * Lookup based implementations are 2 times slower than the others.
  *
@@ -167,6 +168,25 @@ function create_flat_baseline(fn) {
   };
 }
 
+function create_flat_chunks(fn) {
+  let chunks = [];
+  for (let size of CHUNK_SIZES) {
+    let chunk = [];
+    for (let i = 0; i < size; i++) {
+      chunk.push({ x: i }, { y: i + 1 });
+    }
+    chunks.push(chunk);
+  }
+
+  return () => {
+    for (let chunk of chunks) {
+      for (let i = 0; i < chunk.length; i += ENTITY_SIZE) {
+        fn(chunk[i], chunk[i + 1]);
+      }
+    }
+  };
+}
+
 function create_flat_lookup(fn) {
   let chunks = [];
   let data = [];
@@ -211,5 +231,6 @@ suite("Structure of Arrays")
 
 suite("Flat array")
   .add("baseline", create_flat_baseline(check))
+  .add("chunks", create_flat_chunks(check))
   .add("lookup", create_flat_lookup(check))
   .run();
