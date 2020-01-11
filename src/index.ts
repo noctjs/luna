dd2338638fcd05ac987ac43cbc55eba0aadb4e33
Florent Cailhol <florent.cailhol@gmail.com>
2020-01-11 15:04:55 +0100

    Separate type exports from functions

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,17 @@
+// API
 export {
   createEntities,
   createEntity,
   deleteEntities,
   deleteEntity,
-  Entity,
   getComponent
 } from "./entity";
-export { createQuery, iterateEntities, iterateQuery, Query } from "./query";
-export {
-  createTypeRegistry,
-  registerComponent,
-  registerType,
-  Type,
-  TypeDefinition,
-  TypeRegistry
-} from "./type";
-export { createWorld, World } from "./world";
+export { createQuery, iterateEntities, iterateQuery } from "./query";
+export { createTypeRegistry, registerComponent, registerType } from "./type";
+export { createWorld } from "./world";
+
+// Types
+export { Entity } from "./entity";
+export { Query } from "./query";
+export { Type, TypeDefinition, TypeRegistry } from "./type";
+export { World } from "./world";
